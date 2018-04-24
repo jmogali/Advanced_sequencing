ccd6d8a9c2223f92c38547ab7d15d83d3c18a431
Jayanth Mogali <jmogali@andrew.cmu.edu>
2018-04-24 19:46:41 -0400

    minor edit

--- include/VLNS/VLNS_Interface.h
+++ include/VLNS/VLNS_Interface.h
@@ -3,6 +3,6 @@
 #define VLNS_INTERFACE_H
 
 void perform_TSP_Move(std::string strTSPFileFolder, std::vector<std::list<size_t>> &rob_seq, const std::vector<std::vector<Vertex_Schedule>> &vec_rob_sch, const Greedy_Heuristic &heur, const Layout_LS &graph, const Enabling_Graph &en_graph, int kVal);
-
+void free_TSP_buffers();
 
 #endif
